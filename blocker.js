81283213d59e4411e66e6e04193310273c7ff479
ericpa06 <ericpa06@gmail.com>
2025-11-18 21:02:37 -0300

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,8 +1,26 @@
-/// blocker.js
-(function() {
-    'use strict';
-    console.log("%c █ BLOCKER.JS LOADED SUCCESSFULLY VIA uBLOCK █", "background:#f00;color:#fff;font-size:24px;padding:10px");
-    console.log("Timestamp:", new Date().toISOString());
-    console.log("Page URL:", location.href);
-    alert("blocker.js is running via uBlock Origin right now!");
-})();
+/// disable-pageview-api.js
+// Based on: https://addons.mozilla.org/firefox/addon/disable-page-visibility/
+// License:  http://www.opensource.org/licenses/bsd-license.php
+(function(){
+alert("blocker.js is running via uBlock Origin right now!");
+// visibilitychange events are captured and stopped 
+document.addEventListener("visibilitychange", function(e) {
+    e.stopImmediatePropagation();
+}, true);
+// document.visibilityState always returns false
+Object.defineProperty(Document.prototype, "hidden", {
+    get: function hidden() {
+        return false;
+    },
+    enumerable: true,
+    configurable: true
+});
+// document.visibilityState always returns "visible"
+Object.defineProperty(Document.prototype, "visibilityState", {
+    get: function visibilityState() {
+        return "visible";
+    },
+    enumerable: true,
+    configurable: true
+});
+})()
